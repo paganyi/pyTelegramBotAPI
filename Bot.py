26622c4c5d9cdd41229b291183169e7e3d7ed5fa
paganyi <paganyi@gmail.com>
2017-06-20 23:45:00 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-import config
+import time
 import telebot
+from lesson_01 import config
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
